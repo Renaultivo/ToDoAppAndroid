d01dcb4bb2b564b7a1d7f0c99db3a3abb837612d
Renault Scabora <renaultscabora@hotmail.com>
2023-03-02 22:33:26 -0300

    first commit

---
+++ app/src/main/java/com/renaultivo/todo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.renaultivo.todo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.actitivty_main);
+
+
+
+    }
+
+}
