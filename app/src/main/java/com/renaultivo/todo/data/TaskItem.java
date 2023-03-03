19960be064dcce15d3fa147251d12b620f3ae2a5
Renault Scabora <renaultscabora@hotmail.com>
2023-03-03 20:03:59 -0300

    Add activity_main.xml

---
+++ app/src/main/java/com/renaultivo/todo/data/TaskItem.java
@@ -0,0 +1,13 @@
+package com.renaultivo.todo.data;
+
+import java.util.Date;
+
+public class TaskItem {
+
+    String title;
+    String description;
+    boolean checked;
+
+    Date created_on;
+
+}
