c2bb8e5992180549aed823bc79cf9ec0a8532ec6
0WiIIian0 <84929084+0WiIIian0@users.noreply.github.com>
2023-03-03 21:19:49 -0300

    Created bankdate

--- app/src/main/java/com/renaultivo/todo/data/TaskItem.java
+++ app/src/main/java/com/renaultivo/todo/data/TaskItem.java
@@ -4,10 +4,21 @@ import java.util.Date;
 
 public class TaskItem {
 
-    String title;
-    String description;
-    boolean checked;
+    public static final String tableName = "Task";
+    public static final String idColun = "idTask";
+    public static final String titleColun = "title";
+    public static final String descriptionColun = "description";
+    public static final String checkedColun = "checked";
+    public static final String created_onColun = "created";
 
-    Date created_on;
+    public static final String CreateTable = "CREATE TABLE " + tableName + "("
+            + idColun + " INTEGER PRIMARY KEY," + titleColun + " TEXT,"
+            + descriptionColun + " TEXT," +
+            checkedColun + "BOOLEAN," +
+            created_onColun + " DATE" +")";
+
+    public static final String DropTable = "DROP TABLE IF EXISTS " + tableName;
+
+
 
 }
