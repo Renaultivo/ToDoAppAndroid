813fd5e011b46f24bfb7bedb091f4c3f10be40bc
Renault Scabora <renaultscabora@hotmail.com>
2023-03-03 21:22:52 -0300

    update TaskItem and implement TaskItem-UI-Item

--- app/src/main/java/com/renaultivo/todo/MainActivity.java
+++ app/src/main/java/com/renaultivo/todo/MainActivity.java
@@ -1,17 +1,90 @@
 package com.renaultivo.todo;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.renaultivo.todo.data.TaskItem;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends Activity {
+
+    private LinearLayout buildTaskUI_Item(TaskItem taskItem) {
+
+        LinearLayout layout = new LinearLayout(this);
+        CheckBox checkBox = new CheckBox(this);
+        TextView textView = new TextView(this);
+
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+        layout.setBackgroundColor(Color.parseColor("#222222"));
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+
+        layoutParams.setMargins(30, 10, 30, 10);
+
+        layout.setLayoutParams(layoutParams);
+
+        textView.setTextColor(Color.parseColor("#DDDDDD"));
+
+        checkBox.setChecked(taskItem.checked);
+        textView.setText(taskItem.title);
+
+        textView.setPadding(10, 10, 10, 10);
+
+        layout.addView(checkBox);
+        layout.addView(textView);
+
+        return layout;
+
+    }
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.actitivty_main);
 
+        LinearLayout taskListLayout = findViewById(R.id.taskListLayout);
 
+        ArrayList<TaskItem> taskList = new ArrayList<TaskItem>();
+
+        try {
+            taskList.add(new TaskItem(true, "Primeiro", "Primeiro description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Segundo", "Segundo description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Terceiro", "Terceiro description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Quarto", "Quarto description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Quinto", "Quinto description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Sexto", "Sexto description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Setimo", "Setimo description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Oitavo", "Oitavo description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Nono", "Nono description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Decimo", "Decimo description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Decimo-primeiro", "Decimo-primeiro description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Decimo-Segundo", "Decimo-Segundo description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Decimo-Terceiro", "Decimo-Terceiro description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Decimo-Quarto", "Decimo-Quarto description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+            taskList.add(new TaskItem(true, "Decimo-Quinto", "Decimo-Quinto description", new SimpleDateFormat("yyyy-MM-dd").parse("2023-02-02")));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        for (TaskItem item : taskList) {
+            taskListLayout.addView(buildTaskUI_Item(item));
+        }
+
+        System.out.println(taskList);
 
     }
 
--- app/src/main/java/com/renaultivo/todo/data/TaskItem.java
+++ app/src/main/java/com/renaultivo/todo/data/TaskItem.java
@@ -4,10 +4,17 @@ import java.util.Date;
 
 public class TaskItem {
 
-    String title;
-    String description;
-    boolean checked;
+    public String title;
+    public String description;
+    public boolean checked;
 
-    Date created_on;
+    public Date created_on;
+
+    public TaskItem(boolean checked, String title, String description, Date created_on) {
+        this.checked = checked;
+        this.title = title;
+        this.description = description;
+        this.created_on = created_on;
+    }
 
 }
